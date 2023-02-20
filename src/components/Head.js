5400f67e36d17b6aa3b94718dadeddf2ff423ce4
Komal Choraria <komalchoraria20@gmail.com>
2023-02-20 19:55:09 +0530

    created youtube homepage and watch page

---
+++ src/components/Head.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-10 cursor-pointer"
+          alt="menu"
+          src="https://static.vecteezy.com/system/resources/thumbnails/002/292/406/small/hamburger-menu-line-icon-free-vector.jpg"
+        />
+        <a href="/">
+          <img
+            className="h-12 mx-2"
+            alt="youtube-logo"
+            src="https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6.jpg"
+          />
+        </a>
+      </div>
+      <div className="col-span-10 px-10">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-left-full"
+          type="text"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="user"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
